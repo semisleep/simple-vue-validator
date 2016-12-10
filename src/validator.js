1b6c81f3591b9dd7fe2cbb909f0218635bceed64
semisleep <semisleep@gmail.com>
2016-12-10 16:34:12 +0800

    refactor

---
+++ src/validator.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var _ = require('lodash');
+var utils = require('./utils');
+var Rule = require('./rule');
+
+var Validator = {};
+
+// clone methods from Rule to Validator
+_.keys(Rule.prototype).forEach(function (methodName) {
+  Validator[methodName] = function () {
+    var rule = new Rule();
+    return rule[methodName].apply(rule, arguments);
+  };
+});
+
+Validator.create = function () {
+  return new Rule();
+};
+
+Validator.isEmpty = utils.isEmpty;
+
+Validator.format = utils.format;
+
+Validator.extendTemplates = function(newTemplate) {
+  _.extend(utils.templates, newTemplate);
+};
+
+module.exports = Validator;
