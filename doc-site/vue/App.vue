fc518526e4277200e24deabea988a8b1eb58922f
semisleep <semisleep@gmail.com>
2016-12-10 14:27:32 +0800

    update structure and lib support amd / commonjs / global

---
+++ doc-site/vue/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+    {{say}}
+  </div>
+</template>
+
+<script type="text/javascript">
+  'use strict';
+
+  module.exports = {
+    name: 'App',
+    data: function () {
+      return {say: 'lala'}
+    }
+  }
+</script>
--- src/plugin.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports.say = function() {
-  window.alert('hi');
-};
