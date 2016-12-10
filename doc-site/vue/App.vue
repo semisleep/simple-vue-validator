27b0ba453d61f001f9b51ae736fe7d50c6907ce6
semisleep <semisleep@gmail.com>
2016-12-10 23:37:54 +0800

    more styles

--- doc-site/vue/App.vue
+++ doc-site/vue/App.vue
@@ -4,15 +4,3 @@
     <MainContent />
   </div>
 </template>
-
-<script type="text/javascript">
-  'use strict';
-
-  module.exports = {
-    name: 'App',
-    components: {
-      'LeftNavBar': require('./LeftNavBar.vue'),
-      'MainContent': require('./MainContent.vue')
-    }
-  }
-</script>
