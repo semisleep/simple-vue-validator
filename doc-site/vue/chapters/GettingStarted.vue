50d7b5e89c27e0a956bc0723fd2a1c2e9f2261db
semisleep <semisleep@gmail.com>
2016-12-12 12:27:18 +0800

    demo with code component

--- doc-site/vue/chapters/GettingStarted.vue
+++ doc-site/vue/chapters/GettingStarted.vue
@@ -11,40 +11,8 @@
     <div class="section-content">
       Package is installable via npm.
       <pre><code class="language-dash">npm install --save simple-vue-validator</code></pre>
-      <pre id="testMessage"><code class="language-javascript">{{message}}</code></pre>
+      <pre><code class="language-javascript">window.alert('lala');</code></pre>
     </div>
-    <div class="tab-container">
-      <a href="javascript:void(0)" class="tab active"><span class="hidden-mobile"><i class="fa fa-play"></i>&nbsp;&nbsp;&nbsp;&nbsp;</span>DEMO</a>
-      <a href="javascript:void(0)" class="tab"><span class="hidden-mobile"><i class="fa fa-html5"></i>&nbsp;&nbsp;&nbsp;&nbsp;</span>HTML</a>
-      <a href="javascript:void(0)" class="tab"><span class="hidden-mobile"><i class="fa fa-code"></i>&nbsp;&nbsp;&nbsp;&nbsp;</span>Javascript</a>
-    </div>
-    <div class="page-container">
-      <div class="page active">
-        <BasicExample />
-      </div>
-      <div class="page">
-      </div>
-    </div>
+    <DemoWithCode :components="'BasicExample'" />
   </div>
 </template>
-
-<script type="text/javascript">
-  module.exports = {
-    name: 'GettingStarted',
-    data: function () {
-      return {
-        message: 'alert(\'test\');'
-      };
-    },
-    created: function () {
-      /*
-      window.setTimeout(function () {
-        this.message = 'for (var i = 0; i < 10; i++) { console.log(i); }';
-        this.$nextTick(function () {
-          require('prismjs').highlightElement(this.$el.querySelector("#testMessage"));
-        }.bind(this));
-      }.bind(this), 2000);
-      */
-    }
-  }
-</script>
