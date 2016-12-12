ddd48f2326ca639f9518519825938c458803abd3
semisleep <semisleep@gmail.com>
2016-12-12 21:32:25 +0800

    build-in rule example doc

--- doc-site/vue/chapters/UsagesAndExamples.vue
+++ doc-site/vue/chapters/UsagesAndExamples.vue
@@ -7,6 +7,16 @@
     <div class="section-content">
       <p>Here is an example form with different built-in rules:</p>
       <DemoWithCode :components="'BuiltinRulesExample'"/>
+      <p>
+        This example uses several built-in validation rules, such as
+        <span class="code">required</span>, <span class="code">regex</span>, <span class="code">digit</span>,
+        <span class="code">length</span>, <span class="code">integer</span> and <span class="code">greaterThan</span>,
+        for the full list of built-in rules, check out the <a href="#r_rules">rules API</a>.
+      </p>
+      <p>
+        To reset the validation status, this example calls <span class="code">ValidationBag.reset()</span> method,
+        for detail refer to <a href="#r_validation_bag">validation bag API</a>.
+      </p>
     </div>
     <div class="section-title">Customized Rule</div>
     <div class="section-content">
