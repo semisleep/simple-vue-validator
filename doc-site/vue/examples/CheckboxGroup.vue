d1b56faab2bd19e94c42f2e5c5128ee917e22afa
semisleep <semisleep@gmail.com>
2016-12-13 11:07:26 +0800

    custom component example

---
+++ doc-site/vue/examples/CheckboxGroup.vue
@@ -0,0 +1,71 @@
+<template>
+  <div :class="{'inline':inline, 'block':!inline}">
+    <div class="i-checks" :class="{'i-checks-inline':inline}" v-for="item in items">
+      <label>
+        <input type="checkbox" :name="name" :value="item.value">
+        {{ item.label }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+  'use strict';
+
+  var _ = require('lodash');
+  var $ = require('jquery');
+  require('icheck');
+
+  module.exports = {
+    name: 'Radio',
+    props: ['inline', 'name', 'value', 'items'],
+    watch: {
+      value: function(newValue) {
+        this.updateValue(newValue);
+      }
+    },
+    methods: {
+      updateValue: function(newValues) {
+        var el = $(this.$el);
+        var inputs = el.find('input');
+        inputs.each(function(index, input) {
+          input = $(input);
+          if (_.some(newValues, function(newValue) {
+              return newValue === input.val();
+            })) {
+            input.iCheck('check');
+          } else {
+            input.iCheck('uncheck');
+          }
+        });
+      }
+    },
+    mounted: function () {
+      var el = $(this.$el);
+      var labels = el.find('label');
+      var inputs = labels.find('input');
+      labels.on('ifChanged', function () {
+        var values = [];
+        inputs.each(function(index, input) {
+          input = $(input);
+          if (input.prop('checked')) {
+            values.push(input.val());
+          }
+        });
+        this.$emit('input', values);
+      }.bind(this));
+      inputs.iCheck({
+        checkboxClass: 'icheckbox_square-green',
+        radioClass: 'iradio_square-green'
+      });
+      this.updateValue(this.value);
+    },
+    beforeDestroy: function() {
+      var el = $(this.$el);
+      var labels = el.find('label');
+      var inputs = labels.find('input');
+      labels.off('ifchanged');
+      inputs.iCheck('destroy');
+    }
+  }
+</script>
---
+++ doc-site/vue/examples/CustomComponentExample.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="layout-form">
+    <div class="form-group" :class="{error: validation.hasError('colors')}">
+      <div class="label">* Colors</div>
+      <div class="content">
+        <CheckboxGroup inline="true" :items="[
+        {label:'red', value:'red'},
+        {label:'green', value:'green'},
+        {label:'blue', value:'blue'},
+        {label:'white', value:'white'},
+        {label:'black', value:'black'},
+        ]" v-model="colors" />
+      </div>
+      <div class="message">{{ validation.firstError('colors') }}</div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+  var Vue = require('vue');
+  var SimpleVueValidation = require('../../../src/');
+  var Validator = SimpleVueValidation.Validator;
+
+  Vue.use(SimpleVueValidation);
+
+  module.exports = {
+    data: function () {
+      return {
+        colors: []
+      };
+    },
+    validators: {
+      colors: function (value) {
+        return Validator.value(value).required().size(2);
+      }
+    }
+  }
+</script>
