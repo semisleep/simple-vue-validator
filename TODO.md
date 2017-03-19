63577b7154b9a16d8fedf388ead8fe0c1ff0401f
semisleep <semisleep@gmail.com>
2017-03-19 21:05:46 +0800

    fix #11

--- TODO.md
+++ TODO.md
@@ -1,5 +1,6 @@
 ## TODO
 
+* debounced validator not cancelled after validation.reset
 * set isPassed only if there's actually some operation in rule
 
 ## DONE
--- src/mixin.js
+++ src/mixin.js
@@ -8,78 +8,7 @@ var mixin = {
   Promise: null,
 
   created: function () {
-    // validate methods contains all application validate codes
-    var validateMethods = {};
-    this.$options.validateMethods = validateMethods;
-    var unwatchCallbacks = [];
-    this.$options.unwatchCallbacks = unwatchCallbacks;
-    // generate validate methods and watch properties change for validators
-    var validators = this.$options.validators;
-    if (validators) {
-      Object.keys(validators).forEach(function (key) {
-        var properties = key.split(',');
-        properties = properties.map(function (property) {
-          return property.trim();
-        });
-        var getters = properties.map(function (property) {
-          return generateGetter(this, property);
-        }, this);
-        var validator = validators[key];
-        var options = {};
-        if (!utils.isFunction(validator)) {
-          options = utils.omit(validator, 'validator');
-          validator = validator.validator;
-        }
-        if (options.cache) {
-          // cache the validation result, so that async validator can be fast when submitting the form
-          var option = options.cache === 'last' ? 'last' : 'all';
-          validator = cache(validator, option);
-        }
-        var validateMethod = function () {
-          var args = getters.map(function (getter) {
-            return getter();
-          });
-          var rule = validator.apply(this, args);
-          if (rule) {
-            if (!rule._field) {
-              // field defaults to the first property
-              rule.field(properties[0]);
-            }
-            return this.validation.checkRule(rule);
-          } else {
-            return getPromise().resolve(false);
-          }
-        }.bind(this);
-
-        // add to validate method list
-        validateMethods[properties[0]] = validateMethod;
-
-        // watch change and invoke validate method
-        var validateMethodForWatch = validateMethod;
-        if (options.debounce) {
-          // TODO what if custom field name is used?
-          var decoratedValidateMethod = function () {
-            if (decoratedValidateMethod.sessionId !== this.validation.sessionId) {
-              // skip validation if it's reset before
-              return getPromise().resolve(false);
-            }
-            return validateMethod.apply(this, arguments);
-          }.bind(this);
-          var debouncedValidateMethod = utils.debounce(decoratedValidateMethod, parseInt(options.debounce));
-          var field = properties[0];
-          validateMethodForWatch = function () {
-            // eagerly resetting passed flag if debouncing is used.
-            this.validation.resetPassed(field);
-            // store sessionId
-            decoratedValidateMethod.sessionId = this.validation.sessionId;
-            debouncedValidateMethod.apply(this, arguments);
-          }.bind(this);
-        }
-        watchProperties(this, properties, validateMethodForWatch).forEach(function (unwatch) {
-          unwatchCallbacks.push(unwatch);
-        });
-      }, this);
-    }
+    this.$setValidators(this.$options.validators);
 
     if (this.validation) {
       // set vm to validation
@@ -88,9 +17,7 @@ var mixin = {
   },
 
   beforeDestroy: function () {
-    this.$options.unwatchCallbacks.forEach(function (unwatch) {
-      unwatch();
-    });
+    unwatch(this.$options.validatorsUnwatchCallbacks);
   },
 
   data: function () {
@@ -103,6 +30,80 @@ var mixin = {
   },
 
   methods: {
+    $setValidators: function (validators) {
+      unwatch(this.$options.validatorsUnwatchCallbacks);
+      // validate methods contains all application validate codes
+      var validateMethods = {};
+      this.$options.validateMethods = validateMethods;
+      var unwatchCallbacks = [];
+      this.$options.validatorsUnwatchCallbacks = unwatchCallbacks;
+      // generate validate methods and watch properties change for validators
+      if (validators) {
+        Object.keys(validators).forEach(function (key) {
+          var properties = key.split(',');
+          properties = properties.map(function (property) {
+            return property.trim();
+          });
+          var getters = properties.map(function (property) {
+            return generateGetter(this, property);
+          }, this);
+          var validator = validators[key];
+          var options = {};
+          if (!utils.isFunction(validator)) {
+            options = utils.omit(validator, 'validator');
+            validator = validator.validator;
+          }
+          if (options.cache) {
+            // cache the validation result, so that async validator can be fast when submitting the form
+            var option = options.cache === 'last' ? 'last' : 'all';
+            validator = cache(validator, option);
+          }
+          var validateMethod = function () {
+            var args = getters.map(function (getter) {
+              return getter();
+            });
+            var rule = validator.apply(this, args);
+            if (rule) {
+              if (!rule._field) {
+                // field defaults to the first property
+                rule.field(properties[0]);
+              }
+              return this.validation.checkRule(rule);
+            } else {
+              return getPromise().resolve(false);
+            }
+          }.bind(this);
+
+          // add to validate method list
+          validateMethods[properties[0]] = validateMethod;
+
+          // watch change and invoke validate method
+          var validateMethodForWatch = validateMethod;
+          if (options.debounce) {
+            // TODO what if custom field name is used?
+            var decoratedValidateMethod = function () {
+              if (decoratedValidateMethod.sessionId !== this.validation.sessionId) {
+                // skip validation if it's reset before
+                return getPromise().resolve(false);
+              }
+              return validateMethod.apply(this, arguments);
+            }.bind(this);
+            var debouncedValidateMethod = utils.debounce(decoratedValidateMethod, parseInt(options.debounce));
+            var field = properties[0];
+            validateMethodForWatch = function () {
+              // eagerly resetting passed flag if debouncing is used.
+              this.validation.resetPassed(field);
+              // store sessionId
+              decoratedValidateMethod.sessionId = this.validation.sessionId;
+              debouncedValidateMethod.apply(this, arguments);
+            }.bind(this);
+          }
+          watchProperties(this, properties, validateMethodForWatch).forEach(function (unwatch) {
+            unwatchCallbacks.push(unwatch);
+          });
+        }, this);
+      }
+    },
     $validate: function (fields) {
       var validateMethods = this.$options.validateMethods;
       if (utils.isUndefined(fields)) {
@@ -111,7 +112,7 @@ var mixin = {
         });
       } else {
         fields = utils.isArray(fields) ? fields : [fields];
-        validateMethods = fields.map(function(field) {
+        validateMethods = fields.map(function (field) {
           return validateMethods[field];
         });
       }
@@ -131,6 +132,14 @@ var mixin = {
     }
   }
 };
+
+function unwatch(list) {
+  if (list) {
+    list.forEach(function (unwatch) {
+      unwatch();
+    });
+  }
+}
 
 function generateGetter(vm, property) {
   var names = property.split('.');
