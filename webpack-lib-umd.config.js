a33f9243452d447fa2d4761b07230cd5e1aea60b
Nicolas Douillez <n.douillez@gmail.com>
2017-12-13 21:25:56 +0100

    umd plugin build is now available

---
+++ webpack-lib-umd.config.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+   entry: { 'umd-plugin': './src/index.js' },
+   output: {
+      library: 'SimpleVueValidator',
+      libraryTarget: 'umd',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist/',
+      filename: '[name].js',
+      umdNamedDefine: true
+   },
+   externals: {
+      "vue": "Vue"
+   },
+   module: {
+      rules: [
+         /*
+         {
+           test: /\.vue$/,
+           loader: 'vue-loader',
+           options: {
+             // vue-loader options go here
+           }
+         },
+         {
+           test: /\.js$/,
+           loader: 'babel-loader',
+           exclude: /node_modules/
+         }
+         */
+      ]
+   },
+   devServer: {
+      historyApiFallback: true,
+      noInfo: false,
+      hot: true,
+      inline: true,
+      // https: true,
+      port: 8080,
+      contentBase: path.resolve(__dirname, 'test/web')
+   },
+   devtool: '#source-map'
+};
+
+module.exports.plugins = [];
+
+if (process.env.NODE_ENV === 'production') {
+   // http://vue-loader.vuejs.org/en/workflow/production.html
+   module.exports.plugins = (module.exports.plugins || []).concat([
+      new webpack.DefinePlugin({
+         'process.env': {
+            NODE_ENV: '"production"'
+         }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+         compress: {
+            warnings: false
+         }
+      }),
+      new webpack.LoaderOptionsPlugin({
+         minimize: true
+      })
+   ]);
+}
